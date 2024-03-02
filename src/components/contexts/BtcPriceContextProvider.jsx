1139b426b9fac1a4d222c7cc62de6925ebeae86b
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-02 10:25:57 +0100

    ADDS error handling for btc prices data fetching

--- src/components/contexts/BtcPriceContextProvider.jsx
+++ src/components/contexts/BtcPriceContextProvider.jsx
@@ -1,33 +1,50 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import BtcPriceContext from "./BtcPriceContext";
+import { Snackbar } from "@mui/material";
 
 const BtcPriceContextProvider = ({ children }) => {
   const [btcPrice, setBtcPrice] = useState();
+  const [error, setError] = useState(false);
 
   const getValues = async () => {
-    const currencyValuesResponse = await axios.get(
-      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,eur",
-      {
-        headers: {
-          "x-cg-api-key": import.meta.env.COINGECKO_API_KEY,
-        },
-      }
-    );
+    try {
+      const currencyValuesResponse = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,eur",
+        {
+          headers: {
+            "x-cg-api-key": import.meta.env.COINGECKO_API_KEY,
+          },
+        }
+      );
 
-    setBtcPrice({
-      usd: currencyValuesResponse.data.bitcoin.usd,
-      eur: currencyValuesResponse.data.bitcoin.eur,
-    });
+      setBtcPrice({
+        usd: currencyValuesResponse.data.bitcoin.usd,
+        eur: currencyValuesResponse.data.bitcoin.eur,
+      });
+    } catch (err) {
+      setError(true);
+    }
   };
 
   useEffect(() => {
     getValues();
   }, []);
 
+  const handleClose = () => {
+    setError(false);
+  };
+
   return (
     <BtcPriceContext.Provider value={btcPrice}>
       {children}
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={error}
+        onClose={handleClose}
+        autoHideDuration={5000}
+        message="Bitcoin prices could not be loaded. Refresh this page in a while to try again."
+      />
     </BtcPriceContext.Provider>
   );
 };
