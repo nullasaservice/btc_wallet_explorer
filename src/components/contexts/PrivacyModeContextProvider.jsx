53b13ec7c6df773a5bacf4d1c16fcbe9dabe0ee9
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-02 15:34:29 +0100

    ADDS "PrivacyModeService" for storing setting on-device

--- src/components/contexts/PrivacyModeContextProvider.jsx
+++ src/components/contexts/PrivacyModeContextProvider.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PrivacyModeContext from "./PrivacyModeContext";
+import PrivacyModeService from "../../services/PrivacyModeService";
 
 const PrivacyModeContextProvider = ({ children }) => {
-  const [privacyMode, setPrivacyMode] = useState(false);
+  const [privacyMode, setPrivacyMode] = useState();
+
+  useEffect(() => {
+    if (privacyMode == null) {
+      const isEnabled = PrivacyModeService.isEnabled();
+      setPrivacyMode(isEnabled);
+    }
+  }, []);
 
   const togglePrivacyMode = () => {
     setPrivacyMode((b) => !b);
+    PrivacyModeService.toggleSetting();
   };
 
   return (
