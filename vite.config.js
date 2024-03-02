866205fff39332921e370d1d91632045721ce4a4
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-02 15:55:54 +0100

    CHANGES project to be PWA-installable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico"],
+  manifest: {
+    name: "BTC Wallet Explorer",
+    short_name: "btc-explorer",
+    description: "I am a simple btc explorer",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#90caf9",
+    background_color: "#121212",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
