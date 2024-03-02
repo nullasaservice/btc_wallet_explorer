ba5b34fdda7b8b44b20ef833380236e5e6326d3f
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-02 10:50:16 +0100

    ADDS error handling for non-existent addresses

--- src/components/AddressCard.jsx
+++ src/components/AddressCard.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import AddressesService from "../services/AddressesService";
 import BtcPriceContext from "./contexts/BtcPriceContext";
+import { Warning } from "@mui/icons-material";
 
 const AddressInfo = ({ label, value }) => (
   <>
@@ -22,38 +23,48 @@ const AddressInfo = ({ label, value }) => (
 const AddressCard = ({ addressIndex }) => {
   const [addressData, setAddressData] = useState();
   const [loading, setLoading] = useState(true);
+  const [addressNotFound, setAddressNotFound] = useState(false);
   const btcPrices = useContext(BtcPriceContext);
   const MAX_ADDRESS_CHARACTERS = 30;
 
   const fetchAddressData = async () => {
     const address = AddressesService.getAddressWithIndex(addressIndex);
 
-    const addressDataResponse = await axios.get(
-      "https://blockchain.info/balance?active=" + address
-    );
+    try {
+      const addressDataResponse = await axios.get(
+        "https://blockchain.info/balance?active=" + address
+      );
 
-    const addressBalance =
-      addressDataResponse.data[address]["final_balance"] / 100000000;
-
-    const croppedAddress =
-      address.length <= MAX_ADDRESS_CHARACTERS
-        ? address
-        : address.substring(0, MAX_ADDRESS_CHARACTERS) + "...";
-
-    setAddressData({
-      address: croppedAddress,
       // From satoshis to BTC
-      balance: addressBalance,
-      prices: {
-        usd: btcPrices.usd * addressBalance,
-        eur: btcPrices.eur * addressBalance,
-      },
-    });
-    setLoading(false);
+      const addressBalance =
+        addressDataResponse.data[address]["final_balance"] / 100000000;
+
+      const croppedAddress =
+        address.length <= MAX_ADDRESS_CHARACTERS
+          ? address
+          : address.substring(0, MAX_ADDRESS_CHARACTERS) + "...";
+
+      setAddressData({
+        address: croppedAddress,
+        balance: addressBalance,
+        prices: {
+          usd: btcPrices.usd * addressBalance,
+          eur: btcPrices.eur * addressBalance,
+        },
+      });
+    } catch (err) {
+      if (err?.response?.status === 400) {
+        setAddressData({ address });
+        setAddressNotFound(true);
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     setLoading(true);
+    setAddressNotFound(false);
   }, [addressIndex]);
 
   useEffect(() => {
@@ -75,15 +86,35 @@ const AddressCard = ({ addressIndex }) => {
               label={`Address #${addressIndex + 1}`}
               value={addressData.address}
             />
-            <AddressInfo label="Balance in BTC" value={addressData.balance} />
-            <AddressInfo
-              label="Balance in USD"
-              value={addressData.prices.usd}
-            />
-            <AddressInfo
-              label="Balance in EUR"
-              value={addressData.prices.eur}
-            />
+            {addressNotFound ? (
+              <>
+                <Box
+                  display="flex"
+                  width="100%"
+                  justifyContent="center"
+                  marginTop={2}
+                  marginBottom={-2}
+                >
+                  <Warning color="warning" fontSize="large" />
+                </Box>
+                <p>Info for this address could not be found.</p>
+              </>
+            ) : (
+              <>
+                <AddressInfo
+                  label="Balance in BTC"
+                  value={addressData.balance}
+                />
+                <AddressInfo
+                  label="Balance in USD"
+                  value={addressData.prices.usd}
+                />
+                <AddressInfo
+                  label="Balance in EUR"
+                  value={addressData.prices.eur}
+                />
+              </>
+            )}
           </>
         )}
       </CardContent>
