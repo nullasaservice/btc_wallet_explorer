51fdcf0a70bbbb2115f7be578ebe5d2cdc62b0b6
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-06 17:21:46 +0100

    CHANGES "{New,Edit}AddressModal" components to use "BaseAddressModal" component

--- src/components/NewAddressModal.jsx
+++ src/components/NewAddressModal.jsx
@@ -1,45 +1,18 @@
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  TextField,
-} from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React, { useContext, useState } from "react";
 import AddressService from "../services/AddressService";
 import NotificationContext from "./contexts/NotificationContext";
+import BaseAddressModal from "./BaseAddressModal";
+
+const EMPTY_ADDRESS = "";
 
 const NewAddressModal = () => {
-  const EMPTY_ADDRESS = "";
-
   const [showModal, setShowModal] = useState(false);
-  const [isTextFieldTouched, setIsTextFieldTouched] = useState(false);
-  const [address, setAddress] = useState(EMPTY_ADDRESS);
   const setNotification = useContext(NotificationContext);
 
-  const isEmptyAddress = address === EMPTY_ADDRESS;
-  const hasTextFieldError = isEmptyAddress && isTextFieldTouched;
-
-  const handleClose = () => {
-    setAddress(EMPTY_ADDRESS);
-    setIsTextFieldTouched(false);
-    setShowModal(false);
-  };
-
-  const handleSave = () => {
-    if (isEmptyAddress) {
-      setIsTextFieldTouched(true);
-    } else {
-      AddressService.append(address);
-      setNotification("New address added");
-      handleClose();
-    }
-  };
-
-  const handleChange = (event) => {
-    !isTextFieldTouched && setIsTextFieldTouched(true);
-    setAddress(event.target.value);
+  const handleSave = (address) => {
+    AddressService.append(address);
+    setNotification("New address added");
   };
 
   return (
@@ -47,20 +20,12 @@ const NewAddressModal = () => {
       <Button variant="outlined" onClick={() => setShowModal(true)}>
         New address
       </Button>
-      <Dialog open={showModal} onClose={handleClose}>
-        <DialogContent>
-          <TextField
-            label="Address"
-            value={address}
-            error={hasTextFieldError}
-            onChange={handleChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSave}>Save</Button>
-        </DialogActions>
-      </Dialog>
+      <BaseAddressModal
+        showModal={showModal}
+        initialValue={EMPTY_ADDRESS}
+        onSave={handleSave}
+        onClose={() => setShowModal(false)}
+      />
     </Box>
   );
 };
