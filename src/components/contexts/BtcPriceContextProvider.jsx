e463c069b32c50bab532a0ea0cb73d27be153b61
nullasaservice <36648629+nullasaservice@users.noreply.github.com>
2024-03-09 11:53:02 +0100

    ADDS button to refresh prices (#17)

--- src/components/contexts/BtcPriceContextProvider.jsx
+++ src/components/contexts/BtcPriceContextProvider.jsx
@@ -4,19 +4,21 @@ import BtcPriceContext from "./BtcPriceContext";
 import NotificationContext from "./NotificationContext";
 
 const BtcPriceContextProvider = ({ children }) => {
-  const [btcPrice, setBtcPrice] = useState();
+  const [btcPrices, setBtcPrices] = useState();
+  const [loadingPrices, setLoadingPrices] = useState(true);
   const setNotification = useContext(NotificationContext);
 
-  const getValues = async () => {
+  const fetchPrices = async () => {
     try {
       const currencyValuesResponse = await axios.get(
         "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,eur"
       );
 
-      setBtcPrice({
+      setBtcPrices({
         usd: currencyValuesResponse.data.bitcoin.usd,
         eur: currencyValuesResponse.data.bitcoin.eur,
       });
+      setLoadingPrices(false);
     } catch (_) {
       setNotification(
         "Bitcoin prices could not be loaded. Refresh this page in a while to try again."
@@ -24,12 +26,19 @@ const BtcPriceContextProvider = ({ children }) => {
     }
   };
 
+  const refreshPrices = () => {
+    setLoadingPrices(true);
+    fetchPrices();
+  };
+
   useEffect(() => {
-    setNotification && getValues();
+    setNotification && fetchPrices();
   }, [setNotification]);
 
   return (
-    <BtcPriceContext.Provider value={btcPrice}>
+    <BtcPriceContext.Provider
+      value={{ btcPrices, loadingPrices, refreshPrices }}
+    >
       {children}
     </BtcPriceContext.Provider>
   );
