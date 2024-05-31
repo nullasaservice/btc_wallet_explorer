2abad7cd64ebce560da2715763236d6b14666453
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-05-31 07:59:27 +0200

    REMOVES BtcSatsService

--- src/components/contexts/BtcSatsContextProvider.jsx
+++ src/components/contexts/BtcSatsContextProvider.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
 import BtcSatsContext from "./BtcSatsContext";
-import BtcSatsService from "../../services/BtcSatsService";
+
+const LOCAL_STORAGE_KEY = "SHOW_SATS";
 
 const BtcSatsContextProvider = ({ children }) => {
   const [showSats, setShowSats] = useState();
 
   useEffect(() => {
     if (showSats == null) {
-      const shouldShowSats = BtcSatsService.showSats();
-      setShowSats(shouldShowSats);
+      setShowSats(shouldShowSats());
     }
   }, []);
 
+  const shouldShowSats = () => {
+    let showSats = localStorage.getItem(LOCAL_STORAGE_KEY) ?? false;
+
+    // Already has value. Needs to parse from string into JS object.
+    if (typeof showSats === "string") {
+      showSats = showSats === "true" ? true : false;
+    }
+
+    return showSats;
+  };
+
   const toggleSetting = () => {
     setShowSats((b) => !b);
-    BtcSatsService.toggleSetting();
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, !showSats);
   };
 
   return (
