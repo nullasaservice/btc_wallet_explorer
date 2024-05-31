2f87d006862333d601fcc3781cfadf48096270ad
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-05-31 08:04:07 +0200

    REMOVES PrivacyModeService

--- src/components/contexts/PrivacyModeContextProvider.jsx
+++ src/components/contexts/PrivacyModeContextProvider.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
 import PrivacyModeContext from "./PrivacyModeContext";
-import PrivacyModeService from "../../services/PrivacyModeService";
+
+const LOCAL_STORAGE_KEY = "PRIVACY_MODE";
 
 const PrivacyModeContextProvider = ({ children }) => {
   const [privacyMode, setPrivacyMode] = useState();
 
   useEffect(() => {
     if (privacyMode == null) {
-      const isEnabled = PrivacyModeService.isEnabled();
-      setPrivacyMode(isEnabled);
+      setPrivacyMode(isPrivacyModeEnabled());
     }
   }, []);
 
+  const isPrivacyModeEnabled = () => {
+    let privacyMode = localStorage.getItem(LOCAL_STORAGE_KEY) ?? false;
+
+    // Already has value. Needs to parse from string into JS object.
+    if (typeof privacyMode === "string") {
+      privacyMode = privacyMode === "true" ? true : false;
+    }
+
+    return privacyMode;
+  };
+
   const togglePrivacyMode = () => {
     setPrivacyMode((b) => !b);
-    PrivacyModeService.toggleSetting();
+    localStorage.setItem(LOCAL_STORAGE_KEY, !privacyMode);
   };
 
   return (
